72105a59a268ef978435ea00e37d9ce3a888d8d8
Chris.N <cenunez47@gmail.com>
2018-08-30 10:14:25 -0700

    Used .map() to output the state.options array.

--- src/containers/PreQuestion/PreQuestion.js
+++ src/containers/PreQuestion/PreQuestion.js
@@ -5,7 +5,7 @@ import classes from './PreQuestion.css';
 class PreQuestion extends Component {
 
     state = {
-        options: ['school', 'relationship', 'work', 'friend', 'interview', 'performance'],
+        options: ['school', 'relationship', 'work', 'medical', 'money', 'sleep'],
         answers: []
     }
 
@@ -27,38 +27,35 @@ class PreQuestion extends Component {
                 answers: prevAnswers
             })
         }
-        console.log(this.state.answers);
     }
 
     render () {
-        /* TO DO: Iterate through 'options' array via .map to create below structure */
+
+        /* Map this.state.options into checkboxes. */
+        const allOptions = this.state.options.map(option => {
+            return (
+                <label key={option} className={classes.container}>
+                    <input value={option} type="checkbox" onChange={this.answerChangeHandler} />
+                    <span className={classes.checkmark}>{option.replace(/\b\w/g, l => l.toUpperCase())}</span>
+                </label>
+            );
+        });
+
+        /* Output this.state.answers into a string */
+        let answersString;
+        if (this.state.answers.length === 1) {
+            answersString = `How much is ${this.state.answers.join('')} bothering you?`;
+        } else if (this.state.answers.length === 2) {
+            answersString = `How much are ${this.state.answers[0]} and ${this.state.answers[1]} bothering you?`;
+        } else if (this.state.answers.length > 2) {
+            answersString = `How much are ${this.state.answers.slice(0, -1).join(', ')}, and ${this.state.answers.slice(-1)} bothering you?`;
+        }
+
         return (
             <div className={classes.PreQuestion}>
                 <h1>Hey friend, what's troubling you?</h1>
-                <label className={classes.container}>
-                    <input value="work" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>Work</span>
-                </label>
-                <label className={classes.container}>
-                    <input value="relationship" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>Relationship</span>
-                </label>
-                <label className={classes.container}>
-                    <input value="school" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>School</span>
-                </label>
-                <label className={classes.container}>
-                    <input value="friend" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>Friend</span>
-                </label>
-                <label className={classes.container}>
-                    <input value="interview" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>Interview</span>
-                </label>
-                <label className={classes.container}>
-                    <input value="performance" type="checkbox" onChange={this.answerChangeHandler} />
-                    <span className={classes.checkmark}>Performance</span>
-                </label>
+                {allOptions}
+                <p>{answersString}</p>
             </div>
         )
     }
