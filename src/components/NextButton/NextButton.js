6e33b9f1f4cb29955ef5c774e979b060b52f6bff
Chris.N <cenunez47@gmail.com>
2018-09-13 10:05:02 -0700

    Repositioned buttons.

---
+++ src/components/NextButton/NextButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const nextButton = (props) => {
+    return <button style={{textAlign: 'left'}}>+</button>;
+}
+
+export default nextButton;
---
+++ src/components/SkipButton/SkipButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const skipButton = (props) => {
+    return <button style={{textAlign: 'right'}}>-</button>;
+}
+
+export default skipButton;
