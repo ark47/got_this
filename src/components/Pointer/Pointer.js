0008ed1938c26b39d4afa9f6d59104fa9ffa82c2
Chris.N <cenunez47@gmail.com>
2018-10-02 17:33:23 -0700

    Added hot-air balloon pointer.

--- src/components/Pointer/Pointer.js
+++ src/components/Pointer/Pointer.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import Circle from '../../components/Pointers/Circle';
-import Square from '../../components/Pointers/Square';
-import Star from '../../components/Pointers/Star';
-import Triangle from '../../components/Pointers/Triangle';
-import Octagon from '../../components/Pointers/Octagon';
-import Flower from '../../components/Pointers/Flower';
-import McDeeBurger from '../../components/Pointers/McDeeBurger';
+import Balloon from '../../components/Pointers/Balloon';
 
 const pointer = (props) => {
     let selectedCursor;
     switch (props.cursor) {
-        case ('triangle'):
-            selectedCursor = <Triangle />
-            break;
-        case ('square'):
-            selectedCursor = <Square />
-            break;
-        case ('star'):
-            selectedCursor = <Star />
-            break;
-        case ('octagon'):
-            selectedCursor = <Octagon />
-            break;
-        case ('flower'):
-            selectedCursor = <Flower />
+        case ('balloon'):
+            selectedCursor = <Balloon />
             break;
         case ('circle'):
             selectedCursor = <Circle />
-            break;
-        case ('mcdeeburger'):
-            selectedCursor = <McDeeBurger />
             break;
         default:
             selectedCursor = null;
--- src/components/PointerOptions/PointerOptions.js
+++ src/components/PointerOptions/PointerOptions.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import classes from './PointerOptions.css';
 import Circle from '../Pointers/Circle';
-import Square from '../Pointers/Square';
-import Star from '../Pointers/Star';
-import Triangle from '../Pointers/Triangle';
-import Octagon from '../Pointers/Octagon';
-import Flower from '../Pointers/Flower';
-import McDeeBurger from '../Pointers/McDeeBurger';
+import Balloon from '../Pointers/Balloon';
 
 const pointerOptions = (props) => {
     return (
     <div className={classes.PointerOptions} style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
         <ul>
             <li onClick={() => props.cursorSelect('circle')}><svg height="100%" viewBox="0 0 50 50"><Circle /></svg></li>
-            <li onClick={() => props.cursorSelect('square')}><svg height="100%" viewBox="0 0 50 50"><Square /></svg></li>
-            <li onClick={() => props.cursorSelect('star')}><svg height="100%" viewBox="0 0 50 50"><Star /></svg></li>
-            <li onClick={() => props.cursorSelect('triangle')}><svg height="100%" viewBox="0 0 50 50"><Triangle /></svg></li>
-            <li onClick={() => props.cursorSelect('octagon')}><svg height="100%" viewBox="0 0 50 50"><Octagon /></svg></li>
-            <li onClick={() => props.cursorSelect('flower')}><svg height="100%" viewBox="0 0 50 50"><Flower /></svg></li>
-            <li onClick={() => props.cursorSelect('mcdeeburger')}><svg height="100%" viewBox="0 0 50 50"><McDeeBurger /></svg></li>
+            <li onClick={() => props.cursorSelect('balloon')}><svg height="100%" viewBox="0 0 50 50"><Balloon /></svg></li>
+
         </ul>
     </div>
     );
