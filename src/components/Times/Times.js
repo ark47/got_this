fdbe7e6b8c4655f6b576945fa8abb00829a4a930
Chris.N <cenunez47@gmail.com>
2018-11-07 13:48:47 -0800

    Updated time option UI.

--- src/components/Times/Times.js
+++ src/components/Times/Times.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import classes from './Times.css';
+import One from '../Minutes/One';
+import Two from '../Minutes/Two';
+import Three from '../Minutes/Three';
 
 const times = (props) => {
 
     const time = [60, 120, 180]
+    const compTimes = [<One />, <Two />, <Three />]
     // const timeWidth = `${100 / time.length}%`;
 
     const timeOptions = time.map(tyme => {
 
-        let min = Math.floor(tyme / 60);
-        let sec;
+        // let min = Math.floor(tyme / 60);
+        // let sec;
     
-        if (tyme % 60 >= 10) {
-            sec = tyme % 60;
-        } else {
-            sec = '0' + tyme % 60;
-        }
+        // if (tyme % 60 >= 10) {
+        //     sec = tyme % 60;
+        // } else {
+        //     sec = '0' + tyme % 60;
+        // }
 
         return (
-            <li className={classes.Tyme} key={tyme}>
-                <h2 onClick={() => props.times(tyme)}>{`${min}:${sec}`}</h2>
+            <li onClick={() => props.times(tyme)} className={classes.Tyme} key={tyme}>
+                {/* <h2 onClick={() => props.times(tyme)}>{`${min}:${sec}`}</h2> */}
+                {compTimes[time.indexOf(tyme)]}
             </li>
         );
     });
