d93bd26f2c97ec6d3867f639186b73be1be6a894
Chris.N <cenunez47@gmail.com>
2018-11-08 13:29:18 -0800

    Customized timer color depending on theme used.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -4,16 +4,25 @@ import classes from './Timer.css';
 const timer = (props) => {
     let min = Math.floor(props.time / 60);
     let sec;
+    let color;
 
     if (props.time % 60 >= 10) {
         sec = props.time % 60;
     } else {
         sec = '0' + props.time % 60;
     }
+
+    if (props.path === 'sunset') {
+        color = 'rgb(255, 175, 27)';
+    } else if (props.path === 'balloon') {
+        color = 'rgb(99, 190, 175)';
+    } else {
+        color = 'white';
+    }
     
     return (
-        <div className={classes.Timer}>
-            <h2>{min+':'+sec}</h2>
+        <div style={{opacity: props.path ? 1 : 0}} className={classes.Timer}>
+            <h2 style={{color: color}}>{min+':'+sec}</h2>
         </div>
     )
 }
