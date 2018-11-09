7c51695d0478b1e92673da3481078bd75d4947e6
Chris.N <cenunez47@gmail.com>
2018-11-09 15:28:30 -0800

    Added close button to bug form and updated its UI.

--- src/components/BugForm/BugForm.js
+++ src/components/BugForm/BugForm.js
@@ -4,6 +4,7 @@ import classes from './BugForm.css';
 const bugForm = (props) => {
     return (
         <div style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}} className={classes.BugForm}>
+            <div className={classes.Close} onClick={props.close}></div>
             <h2>Report Bug</h2>
             <form>
                 <textarea name="bug" placeholder="Enter details of the issue here."/>
