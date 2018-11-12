dbc37ebf2cb9ac03e08123ee3ae1b4818532a593
Chris.N <cenunez47@gmail.com>
2018-11-11 17:02:20 -0800

    Implemented React-Router-DOM to index.js and app.js files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
+import BugReports from './containers/BugReports/BugReports';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Layout />
+        <Route path="/bugs" component={BugReports} />
+        <Route path="/" exact component={Layout} />
       </div>
     );
   }
